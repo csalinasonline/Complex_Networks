5ae5083930fb2adafad04cddd1e1379359baafc7
Conrad Salinas <csalinasonline@gmail.com>
2020-09-14 21:33:34 -0700

    Changed test data and updated plots.

--- agglomerative_clustering.py
+++ agglomerative_clustering.py
@@ -11,19 +11,20 @@ functions:
 """
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.image as img 
 from dtaidistance import dtw
 from dtaidistance import clustering
 import dtaidistance.dtw_visualisation as dtwvis
 
 def main():
     s = np.array([
-         [0., 0, 1, 2, 1, 0, 1, 0, 0],
-         [0., 1, 2, 0, 0, 0, 0, 0, 0],
-         [1., 2, 0, 0, 0, 0, 0, 1, 1],
-         [0., 0, 1, 2, 1, 0, 1, 0, 0],
-         [0., 1, 2, 0, 0, 0, 0, 0, 0],
-         [1., 2, 0, 0, 0, 0, 0, 1, 1],
-         [1., 2, 0, 0, 0, 0, 0, 1, 1]])
+         np.flip([0., 0, 1, 2, 1, 0, 1, 0, 0, 1]),
+         [0., 1, 2, 0, 0, 0, 0, 0, 0, 1],
+         np.flip([1., 2, 0, 0, 0, 0, 0, 1, 1, 1],0),
+         [0., 0, 1, 2, 1, 0, 1, 0, 0, 1],
+         [0., 1, 2, 0, 0, 0, 0, 0, 0, 1],
+         np.flip([1., 2, 0, 0, 0, 0, 0, 1, 1, 1],0),
+         np.flip([1., 2, 0, 0, 0, 0, 0, 1, 1, 1],0)])
     
     # Custom Hierarchical clustering
     model1 = clustering.Hierarchical(dtw.distance_matrix_fast, {})
@@ -34,9 +35,15 @@ def main():
 
     fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(10, 10))
     show_ts_label = lambda idx: "ts-" + str(idx)
-    model2.plot("hierarchy.jpg", axes=ax, show_ts_label=show_ts_label,
+    model2.plot('hierarchy.jpg', axes=ax, show_ts_label=show_ts_label,
                show_tr_label=True, ts_label_margin=-10,
                ts_left_margin=10, ts_sample_length=1)
+    
+    # reading png image file 
+    im = img.imread('hierarchy.jpg') 
+
+    # show image
+    plt.imshow(im)
     
 if __name__ == "__main__":
     main()
